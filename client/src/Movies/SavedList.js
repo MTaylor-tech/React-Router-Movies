ff6159c8b1e9e8e0d7335294a770c88bafa9a051
Matthew Taylor <majikpig@gmail.com>
2020-03-11 19:32:39 -0400

    more correct use of activeClassName

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -6,12 +6,7 @@ function SavedList (props) {
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => {
-        console.log(props.location.pathname);
-        if (props.location.pathname===`/movies/${movie.id}`) {
-          return (<NavLink key={movie.id} active className='saved-active' to={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></NavLink>)
-        } else {
-          return (<NavLink key={movie.id} to={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></NavLink>)
-        }
+        return <NavLink key={movie.id} activeClassName='saved-active' to={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></NavLink>
       })}
       <NavLink to="/"><div className="home-button">Home</div></NavLink>
     </div>
