cf13eab6e6acb029ea61f02224c63715bff1cbcd
Matthew Taylor <majikpig@gmail.com>
2020-03-10 20:21:42 -0400

    Stretch 4: NavLinks and styling for current path in SavedList NavLinks

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,21 @@ import SavedList from './Movies/SavedList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
+  const [savedIds, setSavedIds] = useState([]);
+  const [currentId, setCurrentId] = useState();
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    if (!savedIds.includes(movie.id)) {
+      setSavedIds([...savedIds, movie.id]);
+      setSavedList( [...savedList, movie] );
+    }
   };
 
   return (
     <div>
-      <SavedList list={savedList} />
+      <SavedList list={savedList} currentId={currentId} />
       <Route exact path="/" component={MovieList} />
-      <Route path="/movies/:id" render={props=><Movie {...props} addToSavedList={addToSavedList} />}/>
+      <Route path="/movies/:id" render={props=><Movie {...props} addToSavedList={addToSavedList} setId={(id)=>setCurrentId(id)} />}/>
     </div>
   );
 };
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {NavLink, withRouter} from 'react-router-dom';
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <Link to={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></Link>
-    ))}
-    <Link to="/"><div className="home-button">Home</div></Link>
-  </div>
-);
+function SavedList (props) {
+  const [currentId, setCurrentId] = useState(props.currentId);
 
-export default SavedList;
+  useEffect(()=>setCurrentId(props.currentId),[props.currentId]);
+
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => {
+        console.log(props.location.pathname);
+        if (props.location.pathname===`/movies/${movie.id}`) {
+          return (<NavLink key={movie.id} active className='saved-active' to={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></NavLink>)
+        } else {
+          return (<NavLink key={movie.id} to={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></NavLink>)
+        }
+      })}
+      <NavLink to="/"><div className="home-button">Home</div></NavLink>
+    </div>
+  );
+}
+
+export default withRouter(SavedList);
